2304c6f169f14a1b6d7273d4c7939199f1088c1b
Flu3nt <gnedyalkov94@gmail.com>
2022-11-03 16:37:21 +0200

    Create basic folder structure, homepage and navbar component

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 - Download Fonts
 
 [Urbanist](https://fonts.google.com/specimen/Urbanist?query=Urbanist)
+
+## Coding
+
+- Create the Home Page inside pages folder
+  - Create the Navbar components inside components folder
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* Navbar */}
+      {/* Slider */}
+      {/* Categories */}
+    </div>
+  )
+}
+
+export default Home
