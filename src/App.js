6aa3e221b729ec2c574277ece392e6b018362d1d
Flu3nt <gnedyalkov94@gmail.com>
2022-11-07 13:16:50 +0200

    Create Cart Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import Home from "./pages/Home"
 import Product from "./pages/Product"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
+import Cart from "./pages/Cart"
 
 function App() {
   return (
     <div className="App">
-      {/* <Register /> */}
-      <Login />
+      <Cart />
     </div>
   )
 }
